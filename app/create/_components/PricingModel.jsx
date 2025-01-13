106585326851170a65654274450c23ac5f6f92c7
harshwardhanpatil-8010 <harshwardhan.patil2005@gmail.com>
2025-01-13 22:07:45 +0530

    new

--- app/create/_components/PricingModel.jsx
+++ app/create/_components/PricingModel.jsx
@@ -11,13 +11,15 @@ function PricingModel() {
         />
         <div className='grid grid-cols-1 md:grid-cols-2 gap-10 mt-5'>
             {Lookup.pricingOption.map((pricing,index)=>(
-               <div className='flex flex-col items-center p-5 border rounded-xl'>   
+               <div key={index} className='flex flex-col items-center p-5 border rounded-xl'>   
                   <Image src={pricing.icon} alt={pricing.title}
                     width={60}
                     height={60}/>
                     <h2 className='font-medium text-2xl'>{pricing.title}</h2>
                     <div>
-                        
+                        {pricing.features.map((feature,index)=>(
+                            
+                        ))}
                     </div>
                     </div>
             ))}
